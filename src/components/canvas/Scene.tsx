4ebd7bcd2c3fdc684b22bbcdb7455abd33c8deb5
akhmadshin <qwasderfasder@gmail.com>
2025-12-06 18:02:42 +0700

    Minor fixes

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,13 +1,18 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas  } from '@react-three/fiber'
 import { Preload } from '@react-three/drei'
 import * as THREE from 'three'
+import type { RootState } from '@react-three/fiber/dist/declarations/src/core/store';
+import type { CanvasProps } from '@react-three/fiber';
+
 import { r3f } from '@/helpers/global'
 
-export default function Scene({ ...props }) {
+const onCreated = (state: RootState) => (state.gl.toneMapping = THREE.AgXToneMapping)
+
+export default function Scene(props: CanvasProps) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
     <Canvas {...props}
-      onCreated={(state) => (state.gl.toneMapping = THREE.AgXToneMapping)}
+      onCreated={onCreated}
     >
       <r3f.Out />
       <Preload all />
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,36 +2,65 @@ import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
+import {  useRef } from 'react';
 import Header from '../components/Header'
-
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+import type {RefObject} from 'react';
+
 
 import type { QueryClient } from '@tanstack/react-query'
-import { Layout } from '@/components/dom/Layout';
+import Scene from '@/components/canvas/Scene.tsx';
 
 interface MyRouterContext {
   queryClient: QueryClient
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-  component: () => (
+  component: () => <App />,
+})
+
+function App() {
+  const ref = useRef<HTMLDivElement>(null)
+  
+  return (
     <>
       <Header />
-      <Layout>
-        <Outlet />
-      </Layout>
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
+      <div
+        ref={ref}
+        style={{
+          position: 'relative',
+          width: ' 100%',
+          height: '100%',
+          overflow: 'auto',
+          touchAction: 'auto',
         }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          TanStackQueryDevtools,
-        ]}
-      />
+      >
+        <Outlet />
+        <Scene
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100vw',
+            height: '100vh',
+            pointerEvents: 'none',
+          }}
+          eventSource={ref as RefObject<HTMLElement>}
+          eventPrefix='client'
+        />
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            TanStackQueryDevtools,
+          ]}
+        />
+      </div>
     </>
-  ),
-})
+  );
+}
